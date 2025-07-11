1306845f7bf6ab094d7a0faa3e8816d3102dbbfa
Amir Pournasserian <amir.pournasserian@live.com>
2025-07-10 20:09:13 -0400

    Refactor UI and introduce database management features
    - Updated `App.razor` to streamline styles and structure.
    - Redesigned `Header.razor` with a new sticky navbar.
    - Revamped `MainLayout.razor` to include a sidebar and new content layout.
    - Removed `MainLayout.razor.css`, `Counter.razor`, `Weather.razor`, `app.css`, and `favicon.png`.
    - Enhanced `Home.razor` with a new card layout for user input.
    - Added `ServerInfo.cs` with a `DatabaseInfo` class for database management.

---
+++ src/DatabaseConnector/Models/ServerInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace DatabaseConnector.Models;
+
+public class DatabaseInfo
+{
+    public string Title { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ConnectionString { get; set; } = default!;
+    private string _ConnectionString { get; set; } = "Server=localhost;Database=FluentCMS;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+
+    public List<TableInfo> Tables { get; set; } = [];
+    public List<ViewInfo> Views { get; set; } = [];
+    public List<StoreProcedureInfo> StoreProcedures { get; set; } = [];
+    public List<FunctionInfo> Functions { get; set; } = [];
+    //public List<TriggerInfo> Triggers { get; set; } = [];
+    //public List<IndexInfo> Indexes { get; set; } = [];
+    //public List<ConstraintInfo> Constraints { get; set; } = [];
+    //public List<SchemaInfo> Schemas { get; set; } = [];
+}
