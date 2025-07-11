35863fe983fc86462149e754c894a77fe5fe8e2e
Amir Pournasserian <amir.pournasserian@live.com>
2025-07-11 10:31:37 -0400

    Refactor database connection handling and UI form
    - Removed private `_ConnectionString` field from `DatabaseInfo` and retained public `ConnectionString` property.
    - Replaced existing form elements in `Home.razor` with an `EditForm` that binds to a new `DatabaseSettings` model, focusing on the `ConnectionString`.
    - Introduced `DatabaseSettings` class in `DatabaseSettings.cs` with properties for `ConnectionString` and `Name`, providing default values.

--- src/DatabaseConnector/Models/ServerInfo.cs
+++ src/DatabaseConnector/Models/ServerInfo.cs
@@ -6,8 +6,6 @@ public class DatabaseInfo
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public string ConnectionString { get; set; } = default!;
-    private string _ConnectionString { get; set; } = "Server=localhost;Database=FluentCMS;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
-
     public List<TableInfo> Tables { get; set; } = [];
     public List<ViewInfo> Views { get; set; } = [];
     public List<StoreProcedureInfo> StoreProcedures { get; set; } = [];
